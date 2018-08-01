7326faf39473f2849e82705e85bf15ea7946c0b9
Tim 'mithro' Ansell <me@mith.ro>
2018-08-01 14:29:12 -0700

    Merge upstream crosstool-ng file changes.
    https://github.com/conda/conda-recipes/tree/master/cross-compilers/crosstool-ng
    Latest commit 9500727  on Jun 22, 2017

--- crosstool-ng-feedstock/recipe/build.sh
+++ crosstool-ng-feedstock/recipe/build.sh
@@ -2,14 +2,11 @@
 
 export EXTRA_CFLAGS="-I${PREFIX}/include -I${PREFIX}/include/ncurses"
 export EXTRA_LDFLAGS="-L${PREFIX}/lib -Wl,-rpath,${PREFIX}/lib -lncursesw"
-# -rpath-link is needed because libncursesw.so dependes on libtinfo.so and
-# configure will fail to find ncurses without it (if using the conda ncurses
-# package).
-export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib"
+export CPPFLAGS="-I${PREFIX}/include -L${PREFIX}/lib"
 if [[ $(uname) == Darwin ]]; then
   export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
 fi
 ./bootstrap
 ./configure --prefix=${PREFIX}
-make -j${CPU_COUNT} ${VERBOSE_AT}
+make V=1
 make install
